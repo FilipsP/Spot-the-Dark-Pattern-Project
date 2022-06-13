e2410dd2a528e2c020e0bfab6dd13bcb153fad2d
Mares1492 <90237423+Mares1492@users.noreply.github.com>
2022-06-13 15:39:57 +0300

    Alert modal animated alpha

--- src/components/modals/AlertModal.jsx
+++ src/components/modals/AlertModal.jsx
@@ -1,12 +1,22 @@
-
-
+import {CSSTransition} from "react-transition-group";
+import {useEffect, useState} from "react";
 
 
 function AlertModal(props) {
 
+    const [startAnimation,setStartAnimation] = useState(false)
+    useEffect(()=>{
+        setStartAnimation(true)
+    },[])
 
     return(
         <div className="modal-background">
+            <CSSTransition
+                in={startAnimation}
+                timeout={300}
+                classNames="animated-profile-sidebar"
+                unmountOnExit
+            >
             <div className='form-container modal'>
                 <p className='note-text'>{props.content}</p>
                 <div className='note-submit-container'>
@@ -14,6 +24,7 @@ function AlertModal(props) {
                     <button className='submit note-submit' type='submit' onClick={ () => {props.closeAlert()}}>OK</button>
                 </div>
             </div>
+            </CSSTransition>
         </div>
     )
 }
